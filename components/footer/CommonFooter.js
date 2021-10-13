b2a7575fbb392ac84664d2eec7cd7ddc1cc3899d
munafarnisha <munafarnisha@arkinfotec.com>
2021-10-13 19:23:43 +0530

    back to top changes

--- components/footer/CommonFooter.js
+++ components/footer/CommonFooter.js
@@ -121,7 +121,7 @@ const RadiusFooter  = (props) => {
                 ))}
 
                 { props.radius && <div className="backtotop" id="backtotop">
-                    <a href="#" onClick={e => e.preventDefault()}>
+                    <a href="#" onClick={e => {e.preventDefault();document.body.scrollTop = document.documentElement.scrollTop = 0;}}>
                     <div className="arrow-bg"> 
                         <svg width="12" height="21" viewBox="0 0 12 21" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M6 20L6 1.25M6 1.25L1 6.25M6 1.25L11 6.25" stroke="" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
