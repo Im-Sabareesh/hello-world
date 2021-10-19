4a562d8adcb8472a5170b5b2acb98905fdeb0636
Prakash Arul Mani <prakash.paramanandam@arkinfotec.com>
2021-10-19 12:30:40 +0530

    initial commit

--- next.config.js
+++ next.config.js
@@ -3,8 +3,18 @@ const path = require('path');
 module.exports = {
     trailingSlash: true,
     reactStrictMode: true,
-    trailingSlash: true,
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
+    },
+    i18n: {
+        /**
+         * Provide the locales you want to support in your application
+         */
+        locales: ["en-US"],
+        /**
+         * This is the default locale you want to be used when visiting
+         * a non-locale prefixed path.
+         */
+        defaultLocale: "en-US",
     },
 };
