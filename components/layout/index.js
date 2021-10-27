274fbd5165cd4ad1f6eabe3167e971dfa3130c52
Athavan <athavan@arkinfotec.com>
2021-10-27 15:41:16 +0530

    #Hide header in empty path

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
 
 const Layout = (props) => {
     const language = props.language;
-
+    const router = useRouter();
+    console.log(router.pathname);
     return (
         <>
-            <Header language={language} />
+            {/* <Header language={language} /> */}
+            {router.pathname === '/' ? null : (<Header language={language} />)}
             {props.children}
             <Footer language={language} />
         </>
