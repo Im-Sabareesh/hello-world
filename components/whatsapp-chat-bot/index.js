9c8ef5afea764bf84eb9b8d92b2e05d37df45928
munafarnisha <munafarnisha@arkinfotec.com>
2021-12-20 18:23:13 +0530

    wtsapp mobile number update

--- components/whatsapp-chat-bot/index.js
+++ components/whatsapp-chat-bot/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 
+import i18next from 'i18next';
 import WhatsAppWidget from "react-whatsapp-chat-widget";
 import "react-whatsapp-chat-widget/index.css";
 
-const WhatsappChatBotComponent = () => (   
-    <WhatsAppWidget 
-        position="left"
-        phoneNo='XXXXXXXXXX'
-        iconColor="white"
-		iconBgColor="rgb(79, 206, 93)"
-        messageBox={true}
-        messageBoxTxt="Hi Team, Is there any related service available ?"
-    />
-);
+const WhatsappChatBotComponent = () => {
+    const t = i18next.t.bind(i18next);
+    return ( 
+        <WhatsAppWidget
+            position="left"
+            phoneNo='919646579412'
+            iconColor="white"
+            iconBgColor="rgb(79, 206, 93)"
+            messageBox={true}
+            messageBoxTxt={t('wtsappMsgBoxTxt')}
+        />
+    );
+}
   
 export default WhatsappChatBotComponent;
