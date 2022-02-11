080c29df00382c97946b64bfbda3f0aacfe70f8e
ArivuAjay <arivazhagan.pandi@arkinfotec.com>
2022-02-11 10:40:41 +0530

    - Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,11 @@
 yarn run build
 
 rsync -av --exclude-from=.deployignore  ./ ubuntu@13.127.48.125:/var/www/chaincode/
+
+ssh ubuntu@13.127.48.125 << EOF
+  # Enter Project Directory
+  cd /var/www/chaincode/
+
+  # Restart pm2
+  pm2 restart 0
+EOF
